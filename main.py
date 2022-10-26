25a7fa56ac96a5483bdfae018440c47469650355
yanis <yann.amrouche@gmail.com>
2022-10-26 09:18:09 +0200

    converting epsg to gps

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # This is a sample Python script.
 import os
-import pyproj
+
 import openpyxl
 import json
+import pyproj
+
 from selenium import webdriver
 from time import sleep
 import csv
@@ -15,9 +17,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-import folium
-import pyproj
-def tranform(x,y):
+def transform(x,y):
     inProj = pyproj.Proj(init='epsg:2154')
     outProj = pyproj.Proj(init='epsg:4326')
     x2, y2 = pyproj.transform(inProj, outProj, x, y)
@@ -30,16 +30,15 @@ if __name__ == '__main__':
 
     print("==================================================================== APPLICATION =======================================================================")
     path = input("Enter the .xlsx file path : ")
-    coord = input("type of coordinate (epsg or gps) :")
+    coord = input("type of coordinates : \n"
+                  "1-EPSG\n"
+                  "2-GPS\n"
+                  "enter a number : ")
     # Create directory
     dirName = 'files'
 
     # Create target Directory
-    try:
-        os.mkdir(dirName)
-    except FileExistsError:
-        pass
-
+    os.mkdir(dirName)
 
 
     # To open the workbook
@@ -62,7 +61,7 @@ if __name__ == '__main__':
     # Création d'un fichier à partir de la liste tableau
 
     tc_coordinates = open('files/tc_coordinates.csv', 'w')
-    print("generating tc_coordinates.csv...")
+
     for i in range(sheet_obj.max_row):
         cell_obj = sheet_obj.cell(row=i + 1, column=10)
         if (cell_obj.value is not None):
@@ -71,10 +70,8 @@ if __name__ == '__main__':
             else:
                 tc_coordinates.write( str(i) + ',' + cell_obj.value + '\n')
     north_coordinates_file = open('files/north_coordinates_file.csv', 'w')
-    print("generating north_coordinates_file.csv...")
     north_coordinates_file.write('id,longitude,latitude\n')
     south_coordinates_file = open('files/south_coordinates_file.csv', 'w')
-    print("generating south_coordinates_file.csv...")
     south_coordinates_file.write('id,longitude,latitude\n')
     north_coord = []
     south_coord = []
@@ -83,38 +80,50 @@ if __name__ == '__main__':
         "type": "FeatureCollection",
         "features": []
     }
-    print("generating the files...")
+
     for i in range(sheet_obj.max_row):
         north = sheet_obj.cell(row=i + 1, column=8)
         south = sheet_obj.cell(row=i + 1, column=9)
         if (north.value is not None or south.value is not None):
             if(not(north.value=='North coordinates')):
                 north_coordinates_file.write(str(i) + ',' + north.value + '\n')
-                north_coord.append(float(str(north.value).split(', ')[1]))
-                north_coord.append(float(str(north.value).split(', ')[0]))
+                if(coord == str(2)):
+                    north_coord.append(float(str(north.value).split(', ')[1]))
+                    north_coord.append(float(str(north.value).split(', ')[0]))
+                if(coord == str(1)):
+                    print('epsg north')
+                    list1 = transform(float(str(north.value).split(', ')[1]), float(str(north.value).split(', ')[0]))
+                    north_coord.append(list1[1])
+                    north_coord.append(list1[0])
             if (not (south.value == 'South coordinates')):
                 south_coordinates_file.write(str(i) + ',' + south.value + '\n')
-                south_coord.append(float(str(south.value).split(', ')[1]))
-                south_coord.append(float(str(south.value).split(', ')[0]))
+                if(coord == str(2)):
+                    south_coordinates_file.write(str(i) + ',' + south.value + '\n')
+                    south_coord.append(float(str(south.value).split(', ')[1]))
+                    south_coord.append(float(str(south.value).split(', ')[0]))
+                if(coord == str(1)):
+                    print('epsg south')
+                    list2 = transform(float(str(south.value).split(', ')[1]), float(str(south.value).split(', ')[0]))
+                    south_coord.append(list2[1])
+                    south_coord.append(list2[0])
                 tmp = {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                        "coordinates": [
-                            north_coord,
-                            south_coord
-                        ],
-                        "type": "LineString"
+                        "type": "Feature",
+                        "properties": {},
+                        "geometry": {
+                            "coordinates": [
+                                north_coord,
+                                south_coord
+                            ],
+                            "type": "LineString"
+                        }
                     }
-                }
 
                 mon_json["features"].append(tmp)
                 with open('files/geojson.json', 'w') as mon_fichier:
-
-                    json.dump(mon_json, mon_fichier,indent=4)
+                        json.dump(mon_json, mon_fichier,indent=4)
                 north_coord = []
                 south_coord = []
-    print("files generated in files directory")
+
     # Print value of cell object
     # using the value attribute
     #print(cell_obj.value)
